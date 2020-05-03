1bf848a83736237b0c843299a8492b63a94d8225
Kamil <kamilwronka7@gmail.com>
2020-05-03 12:03:15 +0200

    Added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,15 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-165394244-1"
+          ></script>
+          <script>
+            {`window.dataLayer = window.dataLayer || []; function gtag()
+            {dataLayer.push(arguments)}
+            gtag('js', new Date()); gtag('config', 'UA-165394244-1');`}
+          </script>
         </Head>
         <body>
           <Main />
